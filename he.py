42652f06a8f4d5453042bba6790beb1c057d0055
ubunturyll <rstevendaryll@gmail.com>
2021-05-01 22:12:23 +1000

    write enhanced images to a file

--- he.py
+++ he.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from skimage import exposure as ex
 import imageio
 import sys
+import cv2 as cv
 
 def he(img):
     if(len(img.shape)==2):      #gray
@@ -17,11 +18,26 @@ def he(img):
     return outImg.astype(np.uint8)
 
 def main():
-    img_name = sys.argv[1]
+    img_name = sys.argv[1] #img as arguments
+    img2 = sys.argv[2]
+    img3 = sys.argv[3]
+
     img = imageio.imread(img_name)
-    result = he(img)
-    plt.imshow(result)
+    read_img2 = imageio.imread(img2)
+    read_img3 = imageio.imread(img3)
+
+    result1 = he(img)
+    result2 = he(read_img2)
+    result3 = he(read_img3)
+
+    plt.imshow(result1)
+    plt.imshow(result2)
+    plt.imshow(result3)
+
     plt.show()
+    cv.imwrite('enhanced1.png',result1)
+    cv.imwrite('enhanced2.png',result2)
+    cv.imwrite('enhanced3.png',result3)
 
 if __name__ == '__main__':
     main()
